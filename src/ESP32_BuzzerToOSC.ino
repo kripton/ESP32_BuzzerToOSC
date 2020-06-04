11b4e0c310b78eeda3f5924c5c294d8e2087a050
Jannis Achstetter <kripton@kripserver.net>
2020-06-04 18:43:20 +0200

    Add sACN recevier. Working with Adafruit NeoPixels

--- src/ESP32_BuzzerToOSC.ino
+++ src/ESP32_BuzzerToOSC.ino
@@ -19,19 +19,23 @@ const int   osc_port = 7703;
 #define     PIN_BUZZER   16
 #define     PIN_LEDSTRIP 21
 #define     NUMLEDS      20
-#define     FIRSTDMXCHAN  1
+
+#define     E131_UNIVERSE  17
+#define     E131_STARTCHAN  1
 
 unsigned long debounceTime = 500; // ms
 
 ///// Globals /////
 OscWiFi osc;
+e131_packet_t e131_packet;
 Adafruit_NeoPixel pixels(NUMLEDS, PIN_LEDSTRIP, NEO_GRB + NEO_KHZ800);
+ESPAsyncE131 e131(1);
 unsigned long lastDetection = 0;
 SemaphoreHandle_t syncSemaphore;
 
 ///// FUNCTIONS /////
 void IRAM_ATTR handleInterrupt() {
-    xSemaphoreGiveFromISR(syncSemaphore, NULL);
+  xSemaphoreGiveFromISR(syncSemaphore, NULL);
 }
 
 void setup() {
@@ -48,6 +52,7 @@ void setup() {
   Serial.println("BUZZER initializing");
 
   // Wifi config + connect
+  WiFi.mode(WIFI_STA);
   WiFi.config(my_ip, my_gw, my_netmask);
   WiFi.begin(wifi_ssid, wifi_password);
   Serial.println("Connecting to WiFi ...");
@@ -72,7 +77,9 @@ void setup() {
   pinMode(PIN_BUZZER, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PIN_BUZZER), handleInterrupt, FALLING);
 
-  // TODO: Set up DMX receiver
+  // Set up DMX receiver
+  //e131.begin(E131_MULTICAST, E131_UNIVERSE, 1); 
+  e131.begin(E131_MULTICAST, E131_UNIVERSE); 
 
   // Clear LEDs
   pixels.clear();
@@ -81,10 +88,34 @@ void setup() {
 
 // Looooooooop
 void loop() {
-  xSemaphoreTake(syncSemaphore, portMAX_DELAY);
+  // DMX input to LEDs
+  Serial.printf("E131 empty: %d\n", e131.isEmpty());
+  if (!e131.isEmpty()) {
+    e131.pull(&e131_packet);     // Pull packet from ring buffer
+
+    Serial.printf("Universe %u / %u Channels | Packet#: %u / Errors: %u / CH1: %u\n",
+      htons(e131_packet.universe),                 // The Universe for this packet
+      htons(e131_packet.property_value_count) - 1, // Start code is ignored, we're interested in dimmer data
+      e131.stats.num_packets,                 // Packet counter
+      e131.stats.packet_errors,               // Packet error counter
+      e131_packet.property_values[1]             // Dimmer data for Channel 1
+    );
+
+    pixels.clear();
+    for(int i = 0; i < NUMLEDS; i++) {
+      int chan = E131_STARTCHAN + i*3;
+      pixels.setPixelColor(i, pixels.Color(e131_packet.property_values[chan], e131_packet.property_values[chan+1], e131_packet.property_values[chan+2]));
+    }
+    pixels.show();
+  }
+
+  //xSemaphoreTake(syncSemaphore, portMAX_DELAY);
+  // Buzzer input
   if (((millis() - lastDetection) > debounceTime) && !gpio_get_level((gpio_num_t)PIN_BUZZER)) {
     Serial.println("Buzzer input detected");
     osc.send(osc_host, osc_port, "/buzzer/rot", 1);
     lastDetection = millis();
   }
+
+  delay(50);
 }
